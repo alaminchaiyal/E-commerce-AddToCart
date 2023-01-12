175596381d38722456f249237421929a3fc2ffb2
Md Al-amin Chaiyal <alaminseiyal2@gmail.com>
2023-01-12 17:20:34 +0600

    header added

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Body.css';
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='product-list'>
+                <p>Products</p>
+            </div>
+            <div className='addToCart'>
+                <p>AddToCarts</p>
+            </div>
+        </div>
+    );
+};
+
+export default Body;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>LapCart BD</h1>
+            <b><p>Buy laptops at affordable price</p></b>
+        </div>
+    );
+};
+
+export default Header;
